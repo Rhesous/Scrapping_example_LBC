1641b4eb4d3483452a8aa81f63b105c7c8b7276b
amaury-rault <amaury.rault@axa.fr>
2018-06-13 12:12:43 +0200

    Collection of more informations

--- lb_scrapper.py
+++ lb_scrapper.py
@@ -25,7 +25,6 @@ class request_lb():
                 Restart with a new database if current already exists
       """
     def __init__(self,request='',
-                 category='',
                  del_prev_db=False,
                  dataloc='./datas/scrapped_lbc.db',
                  dataname='') :
@@ -45,8 +44,12 @@ class request_lb():
         CREATE TABLE IF NOT EXISTS {}(
              link PRIMARY KEY UNIQUE,
              title TEXT,
-             price INTERGER,
+             price INTEGER,
              city TEXT,
+             nb_rooms INTEGER,
+             surface INTEGER,
+             charges TEXT,
+             furnished TEXT,
              description TEXT,
              update_date DATETIME
         )
@@ -55,7 +58,6 @@ class request_lb():
         conn.close()
 
         self.request=request
-        self.category=category
         self.del_prev_db=del_prev_db
         self.dataloc=dataloc
         self.dataname=dataname
@@ -87,8 +89,12 @@ class request_lb():
         CREATE TABLE {}(
              link PRIMARY KEY UNIQUE,
              title TEXT,
-             price INTERGER,
+             price INTEGER,
              city TEXT,
+             nb_rooms INTEGER,
+             surface INTEGER,
+             charges TEXT,
+             furnished TEXT,
              description TEXT,
              update_date DATETIME
         )
@@ -118,7 +124,7 @@ class request_lb():
         i=1
         while(i<=self.nb_iter):
             lbc_page = ("https://www.leboncoin.fr/locations/offres/?o={}".format(nb_page_) +
-                        "&q={}&location=Lyon&mrs=0&mre=2000&sqs=0&sqe=15&ros=1&roe=8&ret=2".format(
+                        "&q={}&location=Lyon&mrs={}&mre={}&sqs={}&sqe={}&ros={}&roe={}&ret=2".format(
                             request_,
                             minrent_,
                             maxrent_,
@@ -138,28 +144,73 @@ class request_lb():
                 if (cursor.fetchone() is None):
                     offer=urllib.request.urlopen(newlink)
                     potage=BeautifulSoup(offer,"lxml")
-                    # Récupération du titre
+
+                    # Title
                     try:
                         title = re.sub('\s\s+', ' ', link.find('p', attrs={'class': 'item_title'}).get_text())
                     except AttributeError:
                         title = re.sub('\s\s+', ' ', link.find('h2', attrs={'class': 'item_title'}).get_text())
                     except:
                         title = "TITLE NOT FOUND"
-                    # Récupération du prix, pas toujours présent
+
+                    # Price
                     if len(link.find_all('p', attrs={'class': "item_price"})) > 0:
                         price = re.sub('\s\s+|\n+', ' ', link.find('p', attrs={'class': "item_price"}).get_text())
                     elif len(link.find_all('h3', attrs={'class': "item_price"})) > 0:
                         price = re.sub('\s\s+|\n+', ' ', link.find('h3', attrs={'class': "item_price"}).get_text())
                     else:
                         price = 'PRICE NOT FOUND'
+
+                    # Import of divs to get refs
+                    refs_ = potage.find_all('div',attrs={'data-reactid':re.compile('\D*')})
+
+                    # Number of rooms
+                    try :
+                        id_ = int([x.get('data-reactid') for x in refs_ if x.getText() == "Pièces"][0]) + 1
+                        nb_rooms_ = potage.find('div',attrs={'class': "_3Jxf3", 'data-reactid': id_}).get_text()
+                        nb_rooms_ = re.sub('\s\s+|\n+', ' ', nb_rooms_)
+                    except Exception:
+                        nb_rooms_ = None
+
+                    # Surface
+                    try:
+                        id_ = int([x.get('data-reactid') for x in refs_ if x.getText() == "Surface"][0]) + 1
+                        surface_ = potage.find('div',
+                                               attrs={'class': "_3Jxf3", 'data-reactid': id_}).get_text()
+                        surface_ = re.sub('\s\s+|\n+', ' ', surface_)
+                    except Exception:
+                        surface_ = None
+
+                    # Charges
+                    try:
+                        id_ = int([x.get('data-reactid') for x in refs_ if x.getText() == "Charges comprises"][0]) + 1
+                        charges_ = potage.find('div',
+                                               attrs={'class': "_3Jxf3", 'data-reactid': id_}).get_text()
+                        charges_ = re.sub('\s\s+|\n+', ' ', charges_)
+                    except Exception:
+                        charges_ = None
+
+                    # Charges
+                    try:
+                        id_ = int([x.get('data-reactid') for x in refs_ if x.getText() == "Meublé / Non meublé"][0]) + 1
+                        furnished_ = potage.find('div',
+                                               attrs={'class': "_3Jxf3", 'data-reactid': id_}).get_text()
+                        furnished_ = re.sub('\s\s+|\n+', ' ', furnished_)
+                    except Exception:
+                        furnished_ = None
+
                     # Récupération de l'adresse
                     city=re.sub("Voir sur la carte","",potage.find('div', attrs={'class':'_1aCZv'}).get_text())
+
                     # Récupération du texte
                     description=re.sub('\s\s+',' ',potage.find('span',attrs={'class':"_2wB1z"}).get_text())
 
                     cursor.execute("""
-                    INSERT INTO {} (link, title, price, city, description,update_date) 
-                    VALUES(?, ?, ?, ?, ?, ?)""".format(self.dataname), (newlink,title,price,city,description, datetime.datetime.now()))
+                    INSERT INTO {} (link, title, price, city, nb_rooms,
+                     surface, charges, furnished, description,update_date) 
+                    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""".format(self.dataname),
+                                   (newlink,title,price,city, nb_rooms_, surface_, charges_, furnished_,
+                                    description, datetime.datetime.now()))
                     if (i%step==0):
                         print('Committing {} new lines'.format(step))
                         evol_ = i/nb_iter*100
@@ -169,7 +220,7 @@ class request_lb():
                     time.sleep(time_sleep_)
 
                 if i-1 == self.nb_iter :
-                    print('Fin des itérations')
+                    print('End of commits')
                     conn.commit()
                     conn.close()
                     return None
@@ -180,6 +231,5 @@ class request_lb():
 if __name__ == '__main__':
     print("Hello world !")
     test = request_lb('T3', True, dataname='Lyon_rent')
-    test.delete_data()
-    test.update_db(step=5,nb_iter=50,time_sleep_=0.01)
+    test.update_db(step=5,nb_iter=50,time_sleep_=0.05)
 
